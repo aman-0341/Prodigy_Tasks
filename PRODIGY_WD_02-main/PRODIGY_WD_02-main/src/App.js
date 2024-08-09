ac3a5a9fefb53474d3d0175b44facb865ca2d5f5
aman_0341 <amanbudhraja18@gmail.com>
2024-08-09 22:59:41 +0530

    Tasks Completed

---
+++ PRODIGY_WD_02-main/PRODIGY_WD_02-main/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import CurrentLocation from "./currentLocation";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="container">
+        <CurrentLocation />
+      </div>
+      <div className="footer-info">
+        <a href="https://github.com/bk408/PRODIGY_WD_02" target="_blank" rel="noreferrer">
+          Download Source Code
+        </a>{" "}
+        | Developed by <a href="">Bhavya Khatri</a>{" "}
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
